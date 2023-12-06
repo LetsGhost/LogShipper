903a2b48a8f16b8f5dbce7a92e886bf67308ad5a
Paul Ehrling <ghostlest330@gmail.com>
2023-12-06 17:27:55 +0100

    Made auth env

--- src/app/Routes/LogRoutes.ts
+++ src/app/Routes/LogRoutes.ts
@@ -1,4 +1,6 @@
 import express from 'express';
+import dotenv from 'dotenv';
+dotenv.config();
 
 import AuthenticateToken from '../Middleware/AuthenticateToken';
 import AuthenticateRole from '../Middleware/AuthenticateRole';
@@ -6,6 +8,11 @@ import AuthenticateRole from '../Middleware/AuthenticateRole';
 import LogController from '../Controller/LogController';
 
 const router = express.Router();
+
+if(process.env.NODE_ENV === 'production') {
+    router.use(AuthenticateToken.authenticateToken);
+    router.use(AuthenticateRole.authenticateRole);
+}
 
 router.get('/getFolderList', LogController.getFolderList);
 router.get('/getLogList/:folderName', LogController.getLogsList);
